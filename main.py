d09cef4b93cee0910a304f571f0aee3ea1e159f6
Jeremy Sharpe <jeremy.adamson.sharpe@gmail.com>
2011-08-23 19:21:23 -0400

    added antialiasing

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from scene import load_scene
 def main(argv):
   raytracer = Raytracer()
   scene = load_scene(os.path.join("data", argv[1]))
-  image = raytracer.trace(scene.get_scene(), (512, 512), 50)
+  image = raytracer.trace(scene.get_scene(), (512, 512), 50, aa=True)
   pygame.image.save(image, os.path.join("results", argv[1] + ".png"))
 
 if __name__ == "__main__":
--- raytracer.py
+++ raytracer.py
@@ -4,6 +4,9 @@ from geometry import Point, Vector, Ray
 def multiplier(fov, value, max_value):
   return -math.tan(fov * (value - max_value / 2.0) / max_value)
 
+def average_colors(colors):
+  return map(lambda x: float(x) / len(colors), reduce(lambda x, y: map(lambda (a, b): a + b, zip(x, y)), colors, (0, 0, 0)))
+
 class Raytracer(object):
   def calculate_color(self, ray, intersection, scene):
     ambient = scene.ambient
@@ -54,10 +57,15 @@ class Raytracer(object):
           closest_dist = dist
       return closest
 
-  def trace(self, scene, resolution, fov):
+  def trace(self, scene, resolution, fov, aa=False):
+    original_resolution = resolution
+    if aa:
+      resolution = map(lambda x: 2 * x, resolution)
     fov_radians = fov * math.pi / 180.0
     image = pygame.Surface(resolution)
     for x in xrange(0, resolution[0]):
+      if x % 100 == 0:
+        print x
       for y in xrange(0, resolution[1]):
         origin = Point(0, 0, 0)
         target_x = Vector(1, 0, 0) * multiplier(fov_radians, x, resolution[0])
@@ -70,4 +78,17 @@ class Raytracer(object):
           image.set_at((x, y), color)
         else:
           image.set_at((x, y), pygame.Color('grey80'))
-    return image
+    if aa:
+      aa_image = pygame.Surface(original_resolution)
+      for x in xrange(0, original_resolution[0]):
+        for y in xrange(0, original_resolution[1]):
+          color = average_colors([
+              image.get_at((2 * x, 2 * y)),
+              image.get_at((2 * x + 1, 2 * y)),
+              image.get_at((2 * x, 2 * y + 1)),
+              image.get_at((2 * x + 1, 2 * y + 1))
+          ])
+          aa_image.set_at((x, y), color)
+      return aa_image
+    else:
+      return image
